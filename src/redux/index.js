67649db6c234084e81045ba01b89a136bc2b0806
osamamammar <osamaammar29@gmail.com>
2022-04-12 19:13:18 +0200

    add getOneSolutionDetailsReducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,14 @@
 // export action creators
 export { getAnonymousUserSolutionsActions } from "./actions/getAnonymousUserSolutionsActions";
 export { getUserProfileCardActions } from "./actions/getUserProfileCardActions";
+export { getOneSolutionDetailsActions } from "./actions/getOneSolutionDetailsActions";
+
 // export constants
 export * from "./constants/getAnonymousUserSolutionsConstants";
 export * from "./constants/getUserProfileCardConstants";
 export * from "./constants/getOneSolutionDetailsConstants";
+
 // export reducers
 export { getAnonymousUserSolutionsReducers } from "./reducers/getAnonymousUserSolutionsReducers";
 export { getUserProfileCardReducers } from "./reducers/getUserProfileCardReducers";
+export { getOneSolutionDetailsReducers } from "./reducers/getOneSolutionDetailsReducers";
--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import {
   getAnonymousUserSolutionsReducers,
+  getOneSolutionDetailsReducers,
   getUserProfileCardReducers,
 } from "./redux";
 
 const reducer = combineReducers({
   getAnonymousUserSolutions: getAnonymousUserSolutionsReducers,
   getUserProfileCard: getUserProfileCardReducers,
+  getOneSolutionDetails: getOneSolutionDetailsReducers,
 });
 const initialState = {};
 const middleware = [thunk];
