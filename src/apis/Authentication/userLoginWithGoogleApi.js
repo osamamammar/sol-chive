9dfd11ade53e314d529e187365a1fe6b75c55689
osamamammar <osamaammar29@gmail.com>
2022-04-22 23:58:28 +0200

    add get all solutions for auth user constants

--- src/apis/Authentication/userLoginWithGoogleApi.js
+++ src/apis/Authentication/userLoginWithGoogleApi.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const userLoginWithGoogleApi = async () => {
   const { data } = await axios.get(
-    `${process.env.REACT_APP_API_URL}/auth/google`
+    `${process.env.REACT_APP_API_URL}/auth/google`,
+    { withCredentials: true }
   );
   return data;
 };
